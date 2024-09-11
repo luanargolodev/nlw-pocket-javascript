58e605c8a34655bf305c4d26afa05490da2427cf
Luan Lemos <ipsluan@icloud.com>
2024-09-11 16:38:47 -0300

    feat(frontend): add mockup

---
+++ web/src/components/create-goal.tsx
@@ -0,0 +1,71 @@
+import { X } from 'lucide-react'
+import { Button } from './ui/button'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from './ui/radio-group'
+
+export function CreateGoal() {
+  return (
+    <DialogContent>
+      <div className="flex flex-col gap-6 h-full">
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <DialogTitle>Cadastrar meta</DialogTitle>
+            <DialogClose>
+              <X className="size-5 text-zinc-600" />
+            </DialogClose>
+          </div>
+
+          <DialogDescription>
+            Adicione atividades que te fazem bem e que você deseja continuar
+            praticando toda semana.
+          </DialogDescription>
+        </div>
+
+        <form action="" className="flex flex-1 flex-col justify-between">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Qual a atividade?</Label>
+              <Input
+                id="title"
+                autoFocus
+                placeholder="Praticar exercícios, meditar, etc..."
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Quantas vezes na semana?</Label>
+              <RadioGroup>
+                <RadioGroupItem value="1">
+                  <RadioGroupIndicator />
+                  <span>1x na semana</span>
+                  <span>🥱</span>
+                </RadioGroupItem>
+              </RadioGroup>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <DialogClose asChild>
+              <Button type="button" className="flex-1" variant="secondary">
+                Fechar
+              </Button>
+            </DialogClose>
+
+            <Button className="flex-1">Salvar</Button>
+          </div>
+        </form>
+      </div>
+    </DialogContent>
+  )
+}
